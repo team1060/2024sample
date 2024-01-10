04b8637088db5a38a9a2ae11c9386818341a47c0
team1060 <team1060dev@gmail.com>
2024-01-10 12:21:41 +0900

    lorem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia iste
+      maiores ullam autem, sunt animi culpa laudantium esse quos porro? Ipsum
+      perspiciatis atque harum consectetur aliquid dignissimos eveniet dolores
+      a!
+    </div>
+  );
 }
 
 export default App;
